693bf32c9f135c6b11e735e97e9630efa828a919
Саша <blackrussiahub1@gmail.com>
2026-09-04 16:54:55 +0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-services:
-  - type: web
-    name: cheboksary-automarket
-    runtime: node
-    buildCommand: npm install
-    startCommand: npm start
-    envVars:
-      - key: NODE_VERSION
-        value: 20
